cf489e5e0f51c8a67e64dca95adbbe05a607b5d7
minikuma <minikuma@naver.com>
2020-09-14 17:24:23 +0900

    ver.1.0.0 finished

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 ### (Study-Project #001) To-do list with Node.js (express)
 
+#### Installation  
+```bash
+git clone https://github.com/minikuma/nodejs-todolist.git  
+npm install
+npm start
+```
+
+#### Tech. Spec
+* Nodejs (with Express)
+* [MongoDB](https://www.mongodb.com/cloud) 
+* EJS (Embedded Javascript Templates)
+
+#### Functions
+|기능|경로|설명|
+|------|---|---|
+|추가|/|텍스트 추가|
+|변경|/edit/:id|등록된 텍스트를 변경|
+|삭제|/remove/:id|등록된 텍스트를 삭제|
+
--- server.js
+++ server.js
@@ -30,48 +30,62 @@ app.use(express.urlencoded({extended: true}));
 // GET Method
 app.get('/', (req, res) => {
     TodoTask.find({}, (err, tasks) => {
-       res.render('todo.ejs', { todoTasks: tasks });
+        res.render('todo.ejs', {todoTasks: tasks});
     });
 });
 
 // POST Method
-app.post('/', async (req, res) => {
-   const todoTask = new TodoTask({
-       content: req.body.content
-   });
-
-   try {
-       await todoTask.save();
-       res.redirect('/');
-   } catch (err) {
-       res.redirect('/');
-   }
-});
+app
+    .post('/', async (req, res) => {
+        const todoTask = new TodoTask({
+            content: req.body.content
+        });
+        try {
+            await todoTask.save();
+            res.redirect('/');
+        } catch (err) {
+            res.redirect('/');
+        }
+    });
 
 // UPDATE Method
 app
     .route('/edit/:id')
     .get((req, res) => {
-         const id = req.params.id;
-         TodoTask.find({}, (err, tasks) => {
-            res.render('todoEdit.ejs', { todoTasks: tasks, idTask: id });
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render('todoEdit.ejs', {todoTasks: tasks, idTask: id});
         });
     })
     .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findOneAndUpdate(id, {content: req.body.content}, err => {
+            if (err) return res.status(500).json({error: 'Update method Fail!'});
+            res.redirect('/');
+        });
+    });
+
+// Delete Method
+app
+    .route('/remove/:id').get((req, res) => {
     const id = req.params.id;
-    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
-        if(err) return res.status(500).json({ error: 'database failure' });
+    TodoTask.findOneAndRemove(id, err => {
+        if (err) return res.status(500).json({error: 'Remove method Fail!'});
         res.redirect('/');
     });
 });
 
-
 // mongoose connection
 mongoose.connect(process.env.DB_CONNECT, {
-        useFindAndModify: true,
-   }, () => {
-    console.log('Connected MongoDB!');
-    app.listen(4000, () => {
-        console.log("Node js Express Sever Stated")
-    });
+    useFindAndModify: false,
+}, function (err) {
+    if (err) {
+        console.error("mongodb connection error!:::", err);
+    } else {
+        console.log('Connected MongoDB!');
+        app.listen(4000, () => {
+            console.log("Node js Express Sever Stated")
+        });
+    }
 });
+
